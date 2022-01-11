7c411e8af9de5f98b07dd83cd5d29770e1bae715
진재 김 <rlawlswo6638@gmail.com>
2022-01-11 09:21:18 +0900

    init update

--- Server/main.cpp
+++ Server/main.cpp
@@ -5,6 +5,20 @@
 
 using namespace std;
 
+void  init() {
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+	SSL_load_error_strings();
+	SSL_library_init();
+	OpenSSL_add_all_algorithms();
+}
+
+void close() {
+	ERR_free_strings();
+	EVP_cleanup();
+	WSACleanup();
+}
+
 int main() {
 
 	system("pause");
